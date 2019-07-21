127b0d37f64d596a0d3d16c94efb72e331c66d69
sergeiudris <sergei.udris@gmail.com>
2019-07-21 14:03:07 +0300

    etl

--- lab/src/lab/imdb/etl.clj
+++ lab/src/lab/imdb/etl.clj
@@ -1,8 +1,39 @@
 (ns lab.imdb.etl
   (:require [clojure.repl :refer :all]
             [clojure.pprint :as pp]
-            [lab.dgraph.core :refer [q set-schema]]
+            [lab.dgraph.core :refer [q create-client set-schema]]
    ;
             )
+  ;
+  )
+
+
+(comment
+
+  (def c (create-client {:with-auth-header? false
+                         :hostname          "server"
+                         :port              9080}))
+
+
+  (->
+   (q {:qstring "{
+  caro(func: allofterms(name@en, \"Marc Caro\")) {
+    name@en
+    director.film {
+      name@en
+    }
+  }
+  jeunet(func: allofterms(name@en, \"Jean-Pierre Jeunet\")) {
+    name@en
+    director.film {
+      name@en
+    }
+  }
+}"
+       :client  c
+       :vars    {}})
+
+   (pp/pprint))
+
   ;
   )
