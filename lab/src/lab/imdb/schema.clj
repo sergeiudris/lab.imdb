aea430e02cb6d1859944c2d581195d00c1f0be1e
sergeiudris <sergei.udris@gmail.com>
2019-07-27 19:43:14 +0300

    rdf

--- lab/src/lab/imdb/schema.clj
+++ lab/src/lab/imdb/schema.clj
@@ -23,16 +23,16 @@
 
 
 ; schema
-(->
- (q {:qstring "schema(pred: [imdb.genre.name, imdb.title.genres imdb.name.primaryName]) {
+  (->
+   (q {:qstring "schema(pred: [imdb.genre.name, imdb.title.genres imdb.name.primaryName]) {
   type
   index
 }"
-     :client  c
-     :vars    {}})
+       :client  c
+       :vars    {}})
 
- (pp/pprint))
-  
+   (pp/pprint))
+
   (def schema-string "
               <imdb.title.averageRating>: float @index(float) .
               <imdb.title.numVotes>: int @index(int) .
@@ -77,55 +77,58 @@
               <imdb.genre.name>: string @index(fulltext,term) @count .
                
               ")
-  
+
   (spit "/opt/.data/imdb.rdf/imdb.schema" schema-string)
 
   (set-schema {:schema-string schema-string
                :client        c})
 
-  (set-schema {:schema-string "
-              <imdb.title.averageRating>: float @index(float) .
-              <imdb.title.numVotes>: int @index(int) .
+  (def schema-string2 "
+              <imdb.title.averageRating>: float .
+              <imdb.title.numVotes>: int  .
               
               <imdb.akas.title>: uid .
-              <imdb.akas.ordering>: int @index(int) .
-              <imdb.akas.titleString>: string @index(exact,fulltext) @count .
-              <imdb.akas.region>: string  @index(term,fulltext) .
-              <imdb.akas.language>: string @index(term) .
-              <imdb.akas.attributes>: [string] @index(term,fulltext) @count .
-              <imdb.akas.isOriginalTitle>: bool @index(bool) .
+              <imdb.akas.ordering>: int .
+              <imdb.akas.titleString>: string  .
+              <imdb.akas.region>: string .
+              <imdb.akas.language>: string .
+              <imdb.akas.attributes>: [string] .
+              <imdb.akas.isOriginalTitle>: bool .
                
-              <imdb.title.titleType>: string @index (term) .
+              <imdb.title.titleType>: string .
               <imdb.title.primaryTitle>: string .
               <imdb.title.originalTitle>: string .
-              <imdb.title.isAdult>: bool @index (bool) .
-              <imdb.title.startYear>: int @index(int) .
-              <imdb.title.endYear>: int @index(int) .
-              <imdb.title.runtimeMinutes>: int @index(int) .
-              <imdb.title.genres>: [string] @index (term,fulltext) .
+              <imdb.title.isAdult>: bool .
+              <imdb.title.startYear>: int .
+              <imdb.title.endYear>: int .
+              <imdb.title.runtimeMinutes>: int .
+              <imdb.title.genres>: uid  .
               
-              <imdb.title.directors>: uid .
-              <imdb.title.writers>: uid .
+              <imdb.title.directors>: uid  .
+              <imdb.title.writers>: uid  .
                
-              <imdb.episode.series>: uid .
-              <imdb.episode.seasonNumber>: int @index (int) .
-              <imdb.episode.episodeNumber>: int @index (int) .
+              <imdb.episode.parentTconst>: uid .
+              <imdb.episode.seasonNumber>: int .
+              <imdb.episode.episodeNumber>: int .
                
               <imdb.principals.title>: uid . 
               <imdb.principals.name>: uid .
-              <imdb.principals.ordering>: int @index (int) .
-              <imdb.principals.category>: string @index(exact) .
-              <imdb.principals.job>: string @index(exact) .
-              <imdb.principals.characters>: string @index(exact,fulltext) .
+              <imdb.principals.ordering>: int .
+              <imdb.principals.category>: string .
+              <imdb.principals.job>: string .
+              <imdb.principals.characters>: string .
                
-              <imdb.name.name>: string @index(exact,fulltext) .
               <imdb.name.primaryName>: string .
-              <imdb.name.birthYear>: int  @index(int) .
-              <imdb.name.deathYear>: int  @index(int) .
-              <imdb.name.primaryProfession>: [string]  @index (term,fulltext) .
+              <imdb.name.birthYear>: int .
+              <imdb.name.deathYear>: int .
+              <imdb.name.primaryProfession>: [string] .
               <imdb.name.knownForTitles>: uid .
                
-              "
+              <imdb.genre.name>: string .
+               
+              ")
+
+  (set-schema {:schema-string schema-string2
                :client        c})
 
 
